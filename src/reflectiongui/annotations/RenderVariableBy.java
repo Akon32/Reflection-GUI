3e3aaeb863d6abd6417e360d9547b1eaf4bf7ea9
Akon32 <akon32@rambler.ru>
2011-02-05 09:20:31 +0300

    Добавлен DefaultMethodRenderer

--- src/reflectiongui/annotations/RenderVariableBy.java
+++ src/reflectiongui/annotations/RenderVariableBy.java
@@ -7,10 +7,12 @@ import java.lang.annotation.*;
 /**
  * Выбор класса, которым нужно отображать
  * аннотированную переменную (поле или параметр метода).
+ * <p/>
+ * Если аннотирован метод, то аннотация относится к его типу результата.
  */
 @Documented
 @Retention(RetentionPolicy.RUNTIME)
-@Target({ElementType.FIELD, ElementType.PARAMETER})
+@Target({ElementType.FIELD, ElementType.PARAMETER, ElementType.METHOD})
 public @interface RenderVariableBy {
     Class<? extends VariableRenderer> value();
 }
---
+++ src/reflectiongui/renderers/standard/DefaultMethodRenderer.java
@@ -0,0 +1,118 @@
+package reflectiongui.renderers.standard;
+
+import reflectiongui.controllers.MethodController;
+import reflectiongui.controllers.VariableController;
+import reflectiongui.renderers.MethodRenderer;
+import reflectiongui.renderers.RendererFactory;
+import reflectiongui.renderers.VariableRenderer;
+import reflectiongui.util.Utils;
+
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.lang.annotation.Annotation;
+
+/** Renderer методов по умолчанию. */
+public class DefaultMethodRenderer implements MethodRenderer {
+    private JComponent rootComponent;
+    private VariableRenderer resultRenderer;
+    private VariableRenderer exceptionRenderer;
+
+    public DefaultMethodRenderer() {
+        rootComponent = new JPanel();
+        rootComponent.setLayout(new BoxLayout(rootComponent, BoxLayout.PAGE_AXIS));
+    }
+
+    @Override
+    public JComponent rootComponent() {
+        return rootComponent;
+    }
+
+    @Override
+    public void displayResult(Object result) {
+        resultRenderer.setValue(result);
+        exceptionRenderer.setValue(null);
+    }
+
+    @Override
+    public void displayException(Throwable throwable) {
+        resultRenderer.setValue(null);
+        exceptionRenderer.setValue(throwable);
+    }
+
+    @Override
+    public void initialize(final MethodController controller) {
+        for (VariableRenderer paramRenderer : controller.getMethodParameters().getRenderers()) {
+            rootComponent.add(paramRenderer.rootComponent());
+        }
+
+        JButton button = new JButton(controller.getMethod().getName());
+        button.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                controller.invoke();
+            }
+        });
+        rootComponent.add(button);
+
+        resultRenderer = RendererFactory.getInstance().createVariableRenderer(controller.getMethod().getReturnType(), controller.getAnnotations());
+        resultRenderer.initialize(new EmptyVariableController(controller.getMethod().getReturnType(), controller.getAnnotations()));
+        resultRenderer.rootComponent().setBorder(BorderFactory.createTitledBorder("Result"));
+        rootComponent.add(resultRenderer.rootComponent());
+
+        // TODO: несколько renderer'ов исключений в методе
+        exceptionRenderer = RendererFactory.getInstance().createVariableRenderer(Throwable.class, new Annotation[0]);
+        exceptionRenderer.initialize(new EmptyVariableController(Throwable.class, new Annotation[0]));
+        exceptionRenderer.rootComponent().setBorder(BorderFactory.createTitledBorder("Error"));
+        rootComponent.add(exceptionRenderer.rootComponent());
+    }
+
+    /**
+     * VariableController, ничего не делающий при вызове методов
+     * {@link #updateObject()}, {@link #updateUI()}
+     */
+    private static class EmptyVariableController implements VariableController {
+        private final Class type;
+        private final Annotation[] annotations;
+
+        private EmptyVariableController(Class type, Annotation[] annotations) {
+            this.type = type;
+            this.annotations = annotations;
+        }
+
+        @Override
+        public void updateObject() {
+            // do nothing
+        }
+
+        @Override
+        public void updateUI() {
+            // do nothing
+        }
+
+        @Override
+        public Class getType() {
+            return type;
+        }
+
+        @Override
+        public boolean isAnnotationPresent(Class<? extends Annotation> annotationClass) {
+            return getAnnotation(annotationClass) != null;
+        }
+
+        @Override
+        public <T extends Annotation> T getAnnotation(Class<T> annotationClass) {
+            return Utils.findObjectOfClass(annotations, annotationClass);
+        }
+
+        @Override
+        public Annotation[] getAnnotations() {
+            return annotations;
+        }
+
+        @Override
+        public Annotation[] getDeclaredAnnotations() {
+            return getAnnotations();
+        }
+    }
+}
