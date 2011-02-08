704f689e3fafe16cb88ad3c02c3783e34a2513c6
Akon32 <akon32@rambler.ru>
2011-02-08 13:42:39 +0300

    Исправлена опечатка в комментарии

--- src/reflectiongui/annotations/Position.java
+++ src/reflectiongui/annotations/Position.java
@@ -2,7 +2,7 @@ package reflectiongui.annotations;
 
 import java.lang.annotation.*;
 
-/** Аннатация, указывающая порядковый номер элемента при отображении. */
+/** Аннотация, указывающая порядковый номер элемента при отображении. */
 @Documented
 @Retention(RetentionPolicy.RUNTIME)
 @Target({ElementType.METHOD, ElementType.FIELD})
