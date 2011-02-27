6e0e95966ca1dd0a8f1212cb94094904f98556fa
Akon32 <akon32@rambler.ru>
2011-02-27 13:10:23 +0300

    DoubleRenderer и StringRenderer переписаны с использованием AbstractSimpleVariableRenderer.

--- src/reflectiongui/renderers/standard/DoubleRenderer.java
+++ src/reflectiongui/renderers/standard/DoubleRenderer.java
@@ -2,42 +2,26 @@ package reflectiongui.renderers.standard;
 
 import reflectiongui.annotations.Renders;
 import reflectiongui.controllers.VariableController;
-import reflectiongui.renderers.VariableRenderer;
 
 import javax.swing.*;
 import javax.swing.text.JTextComponent;
 
 /** Renderer типа для Double */
 @Renders(java.lang.Double.class)
-public class DoubleRenderer implements VariableRenderer {
-    private JPanel panel;
-    private JLabel label;
-    private JTextComponent textComponent;
-
-    public DoubleRenderer() {
-        panel = new JPanel();
-        panel.setLayout(new BoxLayout(panel, BoxLayout.LINE_AXIS));
-        panel.add(label = new JLabel());
-        panel.add(textComponent = new JTextField());
+public class DoubleRenderer extends AbstractSimpleVariableRenderer {
+    @Override
+    protected String objectToString(Object obj) {
+        return obj == null ? null : obj.toString();
     }
 
     @Override
-    public JComponent rootComponent() {
-        return panel;
+    protected Object stringToObject(String str) {
+        str = str.trim();
+        return str.isEmpty() ? null : Double.parseDouble(str);
     }
 
     @Override
-    public void setValue(Object value) {
-        textComponent.setText(value == null ? null : value.toString());
-    }
-
-    @Override
-    public Object getValue() {
-        return textComponent.getText() == null ? null : Double.valueOf(textComponent.getText());
-    }
-
-    @Override
-    public void initialize(VariableController controller) {
-        label.setText(controller.getTitle());
+    protected JTextComponent createTextComponent(VariableController controller) {
+        return new JTextField();
     }
 }
--- src/reflectiongui/renderers/standard/StringRenderer.java
+++ src/reflectiongui/renderers/standard/StringRenderer.java
@@ -1,55 +1,17 @@
 package reflectiongui.renderers.standard;
 
-import reflectiongui.annotations.Multiline;
-import reflectiongui.controllers.VariableController;
-import reflectiongui.renderers.VariableRenderer;
+import reflectiongui.annotations.Renders;
 
-import javax.swing.*;
-import javax.swing.text.JTextComponent;
-
-/**
- * Renderer типа для String.
- * <p/>
- * По умолчанию строка отображается через JTextField.
- * Если указана аннотация {@link reflectiongui.annotations.Multiline},
- * то для отображения используется JTextArea.
- */
-public class StringRenderer implements VariableRenderer {
-    private JPanel panel;
-    private JLabel label;
-    private JTextComponent textComponent;
-
-    public StringRenderer() {
-        panel = new JPanel();
-        panel.setLayout(new BoxLayout(panel, BoxLayout.LINE_AXIS));
+/** Renderer типа для String. */
+@Renders(java.lang.String.class)
+public class StringRenderer extends AbstractSimpleVariableRenderer {
+    @Override
+    protected String objectToString(Object obj) {
+        return (String) obj;
     }
 
     @Override
-    public JComponent rootComponent() {
-        return panel;
-    }
-
-    @Override
-    public void setValue(Object value) {
-        textComponent.setText(value == null ? null : (String) value);
-    }
-
-    @Override
-    public Object getValue() {
-        return textComponent.getText();
-    }
-
-    @Override
-    public void initialize(VariableController controller) {
-        // добавление подписи
-        panel.add(label = new JLabel());
-        label.setText(controller.getTitle());
-        // создание поля
-        if (controller.isAnnotationPresent(Multiline.class)) {
-            textComponent = new JTextArea();
-        } else {
-            textComponent = new JTextField();
-        }
-        panel.add(textComponent);
+    protected Object stringToObject(String str) {
+        return str;
     }
 }
