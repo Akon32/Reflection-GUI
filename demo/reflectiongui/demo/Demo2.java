baa5c9fdae88f0488a7caf55665c2415b8ae543c
Akon32 <akon32@rambler.ru>
2011-03-01 19:52:38 +0300

    Добавлен DesktopPaneDesktopRenderer

---
+++ demo/reflectiongui/demo/Demo2.java
@@ -0,0 +1,20 @@
+package reflectiongui.demo;
+
+import reflectiongui.ReflectionGUI;
+import reflectiongui.renderers.standard.DesktopPaneDesktopRenderer;
+
+/**
+ * Демо ReflectionGUI.
+ * Этот класс служит для запуска программы
+ */
+public class Demo2 {
+
+    public static void main(String[] args) {
+        ReflectionGUI.setDesktopRendererClass(DesktopPaneDesktopRenderer.class);
+        ReflectionGUI.getInstance().addObjects(
+                new Reverser(),
+                new GroupDemo(), new GroupDemo(),
+                new Types(),
+                new Calculator());
+    }
+}
---
+++ src/reflectiongui/renderers/standard/DesktopPaneDesktopRenderer.java
@@ -0,0 +1,88 @@
+package reflectiongui.renderers.standard;
+
+
+import reflectiongui.ReflectionGUI;
+import reflectiongui.annotations.FramePosition;
+import reflectiongui.annotations.FrameSize;
+import reflectiongui.controllers.ObjectController;
+import reflectiongui.grouping.GroupManager;
+import reflectiongui.renderers.DesktopRenderer;
+
+import javax.swing.*;
+import javax.swing.event.InternalFrameAdapter;
+import javax.swing.event.InternalFrameEvent;
+import java.util.HashMap;
+import java.util.Map;
+
+/** DesktopRenderer, отображающий объекты в одном окне. */
+public class DesktopPaneDesktopRenderer implements DesktopRenderer {
+
+    private Map<Object, JInternalFrame> frames = new HashMap<Object, JInternalFrame>();
+    private JDesktopPane desktopPane;
+    private JFrame mainFrame;
+
+    @Override
+    public void addObject(Object object) {
+        if (frames.containsKey(object)) {
+            throw new IllegalArgumentException("Given object is already shown");
+        }
+        if (frames.isEmpty()) {
+            initDesktopPane();
+        }
+        JInternalFrame frame = createFrame(object);
+        frames.put(object, frame);
+        desktopPane.add(frame);
+        frame.setVisible(true);
+    }
+
+    private void initDesktopPane() {
+        mainFrame = new JFrame();
+        mainFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        mainFrame.setVisible(true);
+        mainFrame.setExtendedState(JFrame.MAXIMIZED_BOTH);
+        desktopPane = new JDesktopPane();
+        mainFrame.getContentPane().add(desktopPane);
+    }
+
+    private JInternalFrame createFrame(final Object object) {
+        JInternalFrame frame = new JInternalFrame();
+        // добавление в группы происходит при вызове конструктора ObjectController
+        ObjectController controller = new ObjectController(object);
+        frame.setTitle(controller.getTitle());
+        frame.getContentPane().add(controller.getRenderer().rootComponent());
+        frame.pack();
+        // определение размера из аннотаций
+        if (controller.isAnnotationPresent(FrameSize.class)) {
+            FrameSize s = controller.getAnnotation(FrameSize.class);
+            frame.setSize(s.width(), s.height());
+        }
+        if (controller.isAnnotationPresent(FramePosition.class)) {
+            FramePosition p = controller.getAnnotation(FramePosition.class);
+            frame.setLocation(p.x(), p.y());//разместить по указанным координатам
+        }
+        frame.setClosable(true);
+        frame.setResizable(true);
+        frame.addInternalFrameListener(new InternalFrameAdapter() {
+            @Override
+            public void internalFrameClosing(InternalFrameEvent e) {
+                removeObject(object);
+            }
+        });
+        return frame;
+    }
+
+    @Override
+    public void removeObject(Object object) {
+        JInternalFrame frame = frames.remove(object);
+        if (frame != null) {
+            desktopPane.remove(frame);
+            frame.dispose();
+        }
+        // Удаление из групп
+        GroupManager.getInstance().removeObjectsOfOwner(object);
+        if (frames.isEmpty()) {
+            mainFrame.dispose();
+            ReflectionGUI.getInstance().shutdownApplication();
+        }
+    }
+}
